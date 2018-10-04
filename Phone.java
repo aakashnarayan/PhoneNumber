607ef5adee13dbdde9335a65f08f4a41ed1e8397
aakashnarayan <42978492+aakashnarayan@users.noreply.github.com>
2018-10-03 22:20:37 -0400

    Update Phone.java

--- Phone.java
+++ Phone.java
@@ -6,11 +6,11 @@ public class Phone {
 	public static void main(String[] args) 
 	{
 		Scanner in = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = in.nextLine();
 		System.out.print("Enter your phone number (ex. 4844859997): ");
 		String phoneNumber = in.nextLine();
-		System.out.printf("%-20s" + "(%s)" + "%s-%s", "Phone Number:", phoneNumber.substring(0, 3), phoneNumber.substring(3, 6), phoneNumber.substring(6, 10));
-		
-		
+		System.out.printf("%-20s:" + "(%s)" + "%s-%s", name, phoneNumber.substring(0, 3), phoneNumber.substring(3, 6), phoneNumber.substring(6, 10));
 	}
 
 }
